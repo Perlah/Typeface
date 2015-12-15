b5cce002de5ae6ab8d64860a01afcfa130cad9b7
perlah <herrp830@newschool.edu>
2015-12-15 05:49:28 +0000

    msg ...

--- platz web/typeface/script.js
+++ platz web/typeface/script.js
@@ -6,11 +6,23 @@ $(".circle1").click(function() {
        $("#num").hide();
        $("#punct").hide();
        $("#whole").hide();
+       $("#front").hide();
+       $("#back").hide();
+       $("#post").hide();
+       $("#card").hide();
+       $("#letter").hide();
+      
        console.log("show")
+      
        $(".circle1").css('background', '#DAE0E6');
        $(".circle2").css('background', '#414345');
        $(".circle3").css('background', '#414345');
        $(".circle4").css('background', '#414345');
+       $(".circle5").css('background', '#414345');
+       $(".circle6").css('background', '#414345');
+       $(".circle7").css('background', '#414345');
+       $(".circle8").css('background', '#414345');
+       $(".circle9").css('background', '#414345');
    } 
 });
 
@@ -20,11 +32,23 @@ $(".circle2").click(function() {
        $("#white").hide();
        $("#punct").hide();
        $("#whole").hide();
+       $("#front").hide();
+       $("#back").hide();
+       $("#post").hide();
+       $("#card").hide();
+       $("#letter").hide();
+       
        console.log("num")
+       
        $(".circle2").css('background', '#DAE0E6');
        $(".circle1").css('background', '#414345');
        $(".circle3").css('background', '#414345');
        $(".circle4").css('background', '#414345');
+       $(".circle5").css('background', '#414345');
+       $(".circle6").css('background', '#414345');
+       $(".circle7").css('background', '#414345');
+       $(".circle8").css('background', '#414345');
+       $(".circle9").css('background', '#414345');
    } 
 });
 
@@ -34,11 +58,23 @@ $(".circle3").click(function() {
        $("#num").hide();
        $("#whole").hide();
        $("#white").hide();
+       $("#front").hide();
+       $("#back").hide();
+       $("#post").hide();
+       $("#card").hide();
+       $("#letter").hide();
+       
        console.log("punct")
+       
        $(".circle3").css('background', '#DAE0E6');
        $(".circle2").css('background', '#414345');
        $(".circle1").css('background', '#414345');
        $(".circle4").css('background', '#414345');
+       $(".circle5").css('background', '#414345');
+       $(".circle6").css('background', '#414345');
+       $(".circle7").css('background', '#414345');
+       $(".circle8").css('background', '#414345');
+       $(".circle9").css('background', '#414345');
    } 
 });
 
@@ -48,10 +84,152 @@ $(".circle4").click(function() {
        $("#num").hide();
        $("#white").hide();
        $("#punct").hide();
+       $("#front").hide();
+       $("#back").hide();
+       $("#post").hide();
+       $("#card").hide();
+       $("#letter").hide();
+       
        console.log("whole")
+       
        $(".circle4").css('background', '#DAE0E6');
        $(".circle1").css('background', '#414345');
        $(".circle3").css('background', '#414345');
        $(".circle2").css('background', '#414345');
+       $(".circle5").css('background', '#414345');
+       $(".circle6").css('background', '#414345');
+       $(".circle7").css('background', '#414345');
+       $(".circle8").css('background', '#414345');
+       $(".circle9").css('background', '#414345');
+   } 
+});
+
+$(".circle5").click(function() {
+   if (click == true){
+       $("#whole").hide();
+       $("#num").hide();
+       $("#white").hide();
+       $("#punct").hide();
+       $("#front").show();
+       $("#back").hide();
+       $("#post").hide();
+       $("#card").hide();
+       $("#letter").hide();
+       
+       console.log("front")
+       
+       $(".circle4").css('background', '#414345');
+       $(".circle1").css('background', '#414345');
+       $(".circle3").css('background', '#414345');
+       $(".circle2").css('background', '#414345');
+       $(".circle5").css('background', '#DAE0E6');
+       $(".circle6").css('background', '#414345');
+       $(".circle7").css('background', '#414345');
+       $(".circle8").css('background', '#414345');
+       $(".circle9").css('background', '#414345');
+   } 
+});
+
+$(".circle6").click(function() {
+   if (click == true){
+       $("#whole").hide();
+       $("#num").hide();
+       $("#white").hide();
+       $("#punct").hide();
+       $("#front").hide();
+       $("#back").show();
+       $("#post").hide();
+       $("#card").hide();
+       $("#letter").hide();
+       
+       console.log("back")
+       
+       $(".circle4").css('background', '#414345');
+       $(".circle1").css('background', '#414345');
+       $(".circle3").css('background', '#414345');
+       $(".circle2").css('background', '#414345');
+       $(".circle5").css('background', '#414345');
+       $(".circle6").css('background', '#DAE0E6');
+       $(".circle7").css('background', '#414345');
+       $(".circle8").css('background', '#414345');
+       $(".circle9").css('background', '#414345');
+   } 
+});
+
+$(".circle7").click(function() {
+   if (click == true){
+       $("#whole").hide();
+       $("#num").hide();
+       $("#white").hide();
+       $("#punct").hide();
+       $("#front").hide();
+       $("#back").hide();
+       $("#post").show();
+       $("#card").hide();
+       $("#letter").hide();
+       
+       console.log("post")
+       
+       $(".circle4").css('background', '#414345');
+       $(".circle1").css('background', '#414345');
+       $(".circle3").css('background', '#414345');
+       $(".circle2").css('background', '#414345');
+       $(".circle5").css('background', '#414345');
+       $(".circle6").css('background', '#414345');
+       $(".circle7").css('background', '#DAE0E6');
+       $(".circle8").css('background', '#414345');
+       $(".circle9").css('background', '#414345');
+   } 
+});
+
+$(".circle8").click(function() {
+   if (click == true){
+       $("#whole").hide();
+       $("#num").hide();
+       $("#white").hide();
+       $("#punct").hide();
+       $("#front").hide();
+       $("#back").hide();
+       $("#post").hide();
+       $("#card").show();
+       $("#letter").hide();
+       
+       console.log("card")
+       
+       $(".circle4").css('background', '#414345');
+       $(".circle1").css('background', '#414345');
+       $(".circle3").css('background', '#414345');
+       $(".circle2").css('background', '#414345');
+       $(".circle5").css('background', '#414345');
+       $(".circle6").css('background', '#414345');
+       $(".circle7").css('background', '#414345');
+       $(".circle8").css('background', '#DAE0E6');
+       $(".circle9").css('background', '#414345');
+   } 
+});
+
+$(".circle9").click(function() {
+   if (click == true){
+       $("#whole").hide();
+       $("#num").hide();
+       $("#white").hide();
+       $("#punct").hide();
+       $("#front").hide();
+       $("#back").hide();
+       $("#post").hide();
+       $("#card").hide();
+       $("#letter").show();
+       
+       console.log("letter")
+       
+       $(".circle4").css('background', '#414345');
+       $(".circle1").css('background', '#414345');
+       $(".circle3").css('background', '#414345');
+       $(".circle2").css('background', '#414345');
+       $(".circle5").css('background', '#414345');
+       $(".circle6").css('background', '#414345');
+       $(".circle7").css('background', '#414345');
+       $(".circle8").css('background', '#414345');
+       $(".circle9").css('background', '#DAE0E6');
    } 
 });
